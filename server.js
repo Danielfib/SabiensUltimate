2afe3943a761c3a483306fcb03e07c4072314a67
Mateus Nunes <mateusnunes@nunesmac.local>
2018-11-06 02:14:20 -0300

    botando pra fuder

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(__dirname + "/dist");
+app.use(express.static(__dirname + "/dist"));
+app.get(/.*/, function(req, res) {
+  res.sendfile(__dirname + "/dist/index.html");
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port);
